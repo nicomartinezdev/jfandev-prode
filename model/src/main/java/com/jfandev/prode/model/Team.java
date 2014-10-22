cae7f314405c5936621dca2ebf8c1c17ea46a702
Nicolas Martinez <nicolasalejandromartinez@gmail.com>
2014-10-21 19:37:10 -0700

    Breaking the project into modules and creating a parent pom.

---
+++ model/src/main/java/com/jfandev/prode/model/Team.java
@@ -0,0 +1,33 @@
+package com.jfandev.prode.model;
+
+/**
+ * @author nicolas.martinez
+ */
+
+public class Team {
+
+    private String id;
+    private String nombre;
+    private String simbolo;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getSimbolo() {
+        return simbolo;
+    }
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+    
+    
+}
